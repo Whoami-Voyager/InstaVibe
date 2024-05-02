6524311f9cad3af7d7f10a40d78adb2c84eb3a51
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-05-02 14:10:47 -0600

    changed tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
